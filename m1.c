3a602868320be381795c71ab27538cf3e8e5ee3a
Tejas Tendulkar <tejas.tendulkar@neosoftmail.com>
2022-02-09 16:04:52 +0530

    sys added

--- m1.c
+++ m1.c
@@ -10,5 +10,8 @@ int main() {
     sum = number1 + number2;      
     
     printf("%d + %d = %d", number1, number2, sum);
+    printf("%d + %d = %d", number1, number2, sum);
+    printf("%d + %d = %d", number1, number2, sum);
+    printf("%d + %d = %d", number1, number2, sum);
     return 0;
 }
